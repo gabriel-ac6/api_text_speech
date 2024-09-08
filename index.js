0c733f373d77549a4c88abd7d85dd255710f925a
gabriel-ac6 <95120872+gabriel-ac6@users.noreply.github.com>
2024-09-07 21:02:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import gTTS from 'gtts';
-import multer from 'multer';
-import { PassThrough } from 'stream';
-import ffmpegPath from '@ffmpeg-installer/ffmpeg';
-import ffmpeg from 'fluent-ffmpeg';
-import cors from 'cors';  // Importa o middleware CORS
+const express = require('express');
+const cors = require('cors');
+const gTTS = require('gtts');
+const multer = require('multer');
+const { PassThrough } = require('stream');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
 
-ffmpeg.setFfmpegPath(ffmpegPath.path);
-
-// Configurações do Express
 const app = express();
 const port = 3000;
 
@@ -18,10 +15,8 @@ const upload = multer();
 // Middleware para interpretar o corpo da requisição como JSON
 app.use(express.json());
 
-// Configura o middleware CORS
-app.use(cors({
-    origin: '*',  // Permite todas as origens, ajuste conforme necessário
-}));
+// Habilitando CORS para todas as rotas
+app.use(cors());
 
 // Endpoint para converter texto em áudio e retornar como arquivo
 app.post('/text-to-speech', upload.none(), (req, res) => {
@@ -31,21 +26,19 @@ app.post('/text-to-speech', upload.none(), (req, res) => {
         return res.status(400).json({ error: 'Texto é necessário' });
     }
 
-    // Definindo a língua como português
     const language = 'pt';
     const gtts = new gTTS(text, language);
 
-    // Usar um buffer para capturar o áudio sem salvar em arquivo
     const mp3Stream = new PassThrough();
     gtts.stream().pipe(mp3Stream);
 
-    // Converter o áudio MP3 para WAV usando ffmpeg
     res.set({
         'Content-Type': 'audio/wav',
         'Content-Disposition': 'attachment; filename="audio.wav"'
     });
 
     ffmpeg(mp3Stream)
+        .setFfmpegPath(ffmpegPath)
         .toFormat('wav')
         .on('error', (err) => {
             console.error('Erro ao converter o áudio:', err);
